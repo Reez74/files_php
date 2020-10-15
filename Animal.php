e2f7e817e56cc6e110b57e9330bfbb1ae207928b
Reez74 <71751266+Reez74@users.noreply.github.com>
2020-10-16 02:52:27 +0300

    Create Animal.php
    15.10.2020

---
+++ Animal.php
@@ -0,0 +1,35 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+abstract class Animal{
+  private $nickname;
+  private $age;
+  private $breed;
+  function __construct($nick,$age,$breed){
+    $this->nickname = $nick;
+    $this->age = $age;
+    $this->breed = $breed;
+  }
+  function getNickame(){return $this->nickname;}
+  function getAge(){return $this->age;}
+  function getBreed(){return $this->breed;}
+}
+
+class Horse extends Animal {
+  function run(){ 
+    echo("Игого, я поскакал(а)<br/>"); 
+  }
+} 
+
+class Pegasus extends Horse {
+  function fly(){
+    echo("Игого, я полетел(а)<br/>");
+    
+  }
+}
+
+$agat = new Horse("Agat",7,"Mustang");
+$pegas = new Pegasus("Pegas",8,"Spirit");
+
+echo $agat->run();
+echo $pegas->fly();
+?>
